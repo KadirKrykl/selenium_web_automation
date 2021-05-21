366c5cc14745f65bd08974255d0645f11dfdb805
KadirKrykl <kadirkrykl@gmail.com>
2021-05-22 01:10:08 +0300

    product page update

--- src/main/java/com/testinium/ProductPage.java
+++ src/main/java/com/testinium/ProductPage.java
@@ -11,7 +11,7 @@ public class ProductPage {
     private By price = By.xpath("//*[@id='sp-price-lowPrice']");
     private By addToBasket = By.xpath("//*[@id='add-to-basket']");
     private By basketDiv = By.xpath("//*[@id='header_wrapper']/div[4]/div[3]/a");
-    private By basketButton = By.xpath("//*[@id='add-to-basket']");
+    private By basketButton = By.xpath("//*[@id='header_wrapper']/div[4]/div[3]/div/div/div/div[2]/div[4]/div[1]/a");
 
     public ProductPage(WebDriver driver){
         this.driver = driver;
