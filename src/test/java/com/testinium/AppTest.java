a384110c2e6bb5a341813315b70dc68d538a7b41
KadirKrykl <kadirkrykl@gmail.com>
2021-05-21 18:34:36 +0300

    maven created and dependencies ready

---
+++ src/test/java/com/testinium/AppTest.java
@@ -0,0 +1,26 @@
+package com.testinium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.core.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
